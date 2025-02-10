fb0fe7a81cc6a969472ec3e3e87f42d48321ca1a
Roua-Khalfet <Roua.khalfet@esprit.tn>
2025-02-10 20:41:09 +0100

    prosit 3  done

---
+++ src/Animal.java
@@ -0,0 +1,29 @@
+class Animal {
+
+    String family;
+    String name;
+    int age;
+    boolean isMammal;
+
+
+    public Animal(String family, String name, int age, boolean isMammal) {
+        this.family = family;
+        this.name = name;
+        this.age = age;
+        this.isMammal = isMammal;
+    }
+
+
+    public void displayInfo() {
+        System.out.println("Family: " + family);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Is Mammal: " + isMammal);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Family: " + family + ", Name: " + name + ", Age: " + age + ", Is Mammal: " + isMammal;
+    }
+}
